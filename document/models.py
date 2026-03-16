31b4e09dbc96f2416dedf8bc23d310cbf5609719
gr33nm0On <d2ae358d42d04672@proton.me>
2026-03-16 18:23:14 +0300

    add: +Note model +NoteFile model +Category model

--- document/models.py
+++ document/models.py
@@ -1,3 +1,36 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Note(models.Model):
+    name = models.CharField(max_length=100, validators=[MinLengthValidator(3)])
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        null=False,
+        related_name='notes'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+class NoteFile(models.Model):
+    note = models.ForeignKey(Note, on_delete=models.CASCADE, related_name='files')
+    file = models.FileField(upload_to='notes/files/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.file.name
