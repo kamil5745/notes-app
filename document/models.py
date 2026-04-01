71a86fba2a7c88a6989554c9587388edd464c4ec
анна <grushagaa@gmail.com>
2026-04-01 21:19:05 +0300

    Reset db, migrate with slug

--- document/models.py
+++ document/models.py
@@ -1,17 +1,39 @@
-import datetime
-
+from autoslug import AutoSlugField
+from django.contrib.auth.models import User
 from django.core.validators import MinLengthValidator
 from django.db import models
-from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils.text import slugify
 
-class Category(models.Model):
-    name = models.CharField(max_length=100)
+
+def unicode_slugify(value):
+    return slugify(value, allow_unicode=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
 
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('tag', kwargs={'tag_slug': self.slug})
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_slug': self.slug})
+
+
 class Note(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_notes')
     name = models.CharField(max_length=100, validators=[MinLengthValidator(3)])
     description = models.TextField(blank=True)
     category = models.ForeignKey(
@@ -19,11 +41,16 @@ class Note(models.Model):
         on_delete=models.PROTECT,
         null=True,
         blank=True,
-        related_name='notes'
+        related_name='cat_notes'
     )
-
-    date = models.DateField(null=False, blank=False, default=datetime.date.today)
-
+    tags = models.ManyToManyField('Tag', blank=True, related_name='tag_notes')
+    slug = AutoSlugField(
+        populate_from='name',
+        unique=True,
+        always_update=False,
+        max_length=255,
+        slugify=unicode_slugify
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -32,6 +59,12 @@ class Note(models.Model):
 
     class Meta:
         ordering = ['-created_at']
+        indexes = [
+            models.Index(fields=['created_at'])
+        ]
+
+    def get_absolute_url(self):
+        return reverse('note', kwargs={'note_slug': self.slug})
 
 
 class NoteFile(models.Model):
@@ -46,6 +79,7 @@ class NoteFile(models.Model):
 class Like(models.Model):
     note = models.ForeignKey(Note, on_delete=models.CASCADE, related_name='likes')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+
 
 class View(models.Model):
     note = models.ForeignKey(Note, on_delete=models.CASCADE, related_name='views')
